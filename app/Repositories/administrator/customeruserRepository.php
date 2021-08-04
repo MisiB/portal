c22759896e6eceb1bba04548ba05d87c4dd9d387
Benson Misi <benson.misi@outlook.com>
2021-08-04 11:37:27 +0200

    correct admin user creation module

--- app/Repositories/administrator/customeruserRepository.php
+++ app/Repositories/administrator/customeruserRepository.php
@@ -12,8 +12,17 @@ class customeruserRepository{
        $password = rand(10000,100000);  
        if($request->id=="")
        {
-        User::create(['company_id'=>$request->company,'name'=>$request->name,'surname'=>$request->surname,'email'=>$request->email,'role_id'=>'9','password'=>bcrypt($password)]);
-       }
+           $user = User::with('company')->whereemail($request->email)->first();
+           if(is_null($user))
+           {
+            User::create(['company_id'=>$request->company,'name'=>$request->name,'surname'=>$request->surname,'email'=>$request->email,'role_id'=>'9','password'=>bcrypt($password)]);
+            return array('status'=>'successMessage','message'=>'Successfully Created User temp password:'.$password);
+           }
+           else{
+               $message= !is_null($user->company) ? 'Email already registered to company :'.$user->company->name : 'Email already in our database  cannot be used';
+               return redirect()->back()->with('statusError',$message);
+           }
+        }
        else{
         $user = User::whereid($request->id)->first();
         $user->name = $request->name;
