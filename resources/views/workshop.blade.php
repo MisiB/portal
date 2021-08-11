8fe7173d234085a764047e448b5a7f33cbcd9121
Benson Misi <benson.misi@outlook.com>
2021-08-11 11:09:03 +0200

    bulk email module for mne workshop

---
+++ resources/views/workshop.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>PRAZ Quarterly zoom interactive sessions for Procuring Entities </h2>
+<p>
+The Procurement Regulatory Authority of Zimbabwe Monitoring and Evaluation Division wishes to advise your Procuring Entity that it will be hosting group Quarterly zoom interactive sessions that are focused on discussing with PEs, procurement returns and reports standards and approaches.  The virtual sessions shall be held for respective Procuring Entity categories and are open for the respective Accounting Officers, Heads of PMUs and PMU members to participate. Invites will be sent to registered participants only. (Please complete fully the second table beneath and send back by e-mail to pe@praz.org.zw for your registration)
+</p>
+
+<table>
+ <thead>
+     <tr>
+         <th>Date</th>
+         <th>Time</th>
+         <th>PEs Category</th>
+     </tr>
+ </thead>
+<tbody>
+    <tr><td>Thursday 12 August 2021</td><td>1000-1200hrs</td><td>Commissions- PMU</td></tr>
+    <tr><td>Friday 13 August 2021</td><td>1000-1200hrs</td><td>Ministries- PMU</td></tr>
+    <tr><td>Monday16 August 2021</td><td>1000-1200hrs</td><td>Local Authorities- PMU</td></tr>
+    <tr><td>Wednesday 18 August 2021</td><td>1000-1200hrs</td><td>Local Authorities- Accounting Officers</td></tr>
+    <tr><td>Wednesday 18 August 2021</td><td>1000-1200hrs</td><td>Parastatals- Accounting Officers</td></tr>
+    <tr><td>Thursday 19 August 2021</td><td>1000-1200hrs</td><td>Commissions- Accounting Officers</td></tr>
+    <tr><td>Thursday 19 August 2021</td><td>1000-1200hrs</td><td>Ministries- Accounting Officers</td></tr>
+</tbody>
+</table>
+ <p>The discussion will be limited to the set time and should you have specific questions that you would like the M & E team to prepare responses for, kindly send them through to pe@praz.org.zw at least 4 hours before the meeting starts.  A register will be maintained for the Authority’s reporting purposes. We look forward to your participation.</p>
+ <br/>
+ <table>
+ <thead>
+     <tr>
+         <th>Mr./Mrs./Ms./Miss</th>
+         <th>Surname</th>
+         <th>Name</th>
+         <th>Procuring Entity</th>
+         <th>Title/Position</th>
+         <th>E-mail Address</th>
+     </tr>
+ </thead>
+<tbody>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</tbody>
+ </table>
+ <br/>
+<p>Kindly ensure to include your gender, to enable the Authority’s reporting metrics </p>
+<br/>
+<p>Procurement Regulatory Authority of Zimbabwe (PRAZ)<br/>
+Bank Chambers Building<br/>
+76 Samora Machel Ave<br/>
+Harare.
+</p>
+
+
+</body>
+</html>
